fbc83738abfe064126231859079d49c922e2c6fe
Kuldeep Parmar <kuldeepparmar246@gmail.com>
2025-04-18 00:21:45 +0530

    see

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 import config from './utils/config.js'
 
-app.listen(config.PORT, () => {
+app.listen(config.PORT,'0.0.0.0',() => {
   console.log(`Server running on port ${config.PORT}`)
 })
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv"
-
 dotenv.config()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 const MONGODB_URI = process.env.MONGODB_URI
 
 export default { MONGODB_URI, PORT }
